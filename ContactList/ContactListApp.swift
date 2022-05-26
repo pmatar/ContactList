b05989a225b8d11f6b85e5fcc8e9ffa9439b73b7
Paul Matar <paul_matar@icloud.com>
2022-05-26 14:10:39 +0300

    Initial Commit

---
+++ ContactList/ContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListApp.swift
+//  ContactList
+//
+//  Created by Paul Matar on 26/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
